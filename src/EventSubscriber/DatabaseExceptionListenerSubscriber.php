d922ceec140efe24fa33c329687f708d59d4b662
DatOris <ntdathttt2211003@student.ctuet.edu.vn>
2025-01-08 01:35:14 +0700

    .

---
+++ src/EventSubscriber/DatabaseExceptionListenerSubscriber.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class DatabaseExceptionListenerSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof ConnectionException) {
+            $response = new Response(
+                $this->twig->render('bundles/TwigBundle/error.html.twig'),
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+            $event->setResponse($response);
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+}
